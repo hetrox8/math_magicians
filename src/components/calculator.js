fdb1a69fcbf8de6bfd6de5499b2749aa17bc9a76
suleiman gacheru <dretrevor8@gmail.com>
2023-08-09 14:33:19 +0300

    add calculator components

---
+++ src/components/calculator.js
@@ -0,0 +1,12 @@
+import Button from './button';
+
+export default function calculator() {
+  return (
+    <div className="calculator_container">
+      <div className="calculator">
+        <p className="output">0</p>
+        <Button />
+      </div>
+    </div>
+  );
+}
